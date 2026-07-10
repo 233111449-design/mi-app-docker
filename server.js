9df90dcfdf1af0b59acdf77b2ca049d967a56c28
Rigoberto Angeles <233111449@upmh.edu.mx>
2026-07-09 19:53:02 -0600

    Actualizar página con nombre del alumno

--- server.js
+++ server.js
@@ -5,18 +5,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-    res.send(`
-        <h1>Hola, soy Rigoberto Angeles</h1>
-        <h2>Aplicación desplegada en Render</h2>
-        <p>Esta práctica fue realizada utilizando:</p>
-        <ul>
-            <li>Node.js</li>
-            <li>Express</li>
-            <li>Yarn</li>
-            <li>Docker</li>
-            <li>Render</li>
-        </ul>
-    `);
+    res.send("Hola, soy Rigoberto Angeles. Esta aplicación fue desplegada en Render usando Docker y Yarn 🚀");
 });
 
 app.listen(PORT, () => {
